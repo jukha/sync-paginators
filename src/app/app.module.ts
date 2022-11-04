31a3af929a057f8bfb4b2846e3d7877f00afce54
junaid_07 <junaid@xenex-media.com.au>
2022-11-04 14:59:47 +0500

    setup minimal reproduction of the error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { TableModule } from "primeng/table";
+import { HttpClientModule } from "@angular/common/http";
+import { PaginatorModule } from "primeng/paginator";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, TableModule, HttpClientModule, PaginatorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
